e1524692dd2bbba26dedc1af7c1233ed7f3bc7c9
ahmed-nasir <nasirahmed595@gmail.com>
2022-04-09 21:00:25 +0600

    toggle signout

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import useFirebase from '../../hooks/useFirebase';
 
 const Header = () => {
-    const { user } = useFirebase();
+    const { user, handleSignOut } = useFirebase();
     return (
         <div>
 
@@ -14,10 +14,11 @@ const Header = () => {
                         <Link to='/products' className='pr-4 hover:text-orange-400'>Products</Link>
                         <Link to='/Orders' className='pr-4 hover:text-orange-400'>Orders</Link>
                         <Link to='/register' className='pr-4 hover:text-orange-400'>Register</Link>
+                        <span>{user?.displayName && user?.displayName}</span>
                         {
-                            user.uid
+                            user?.uid
                                 ?
-                                <button>Sign out</button>
+                                <button onClick={handleSignOut}>Sign out</button>
                                 :
                                 <Link to='/login' className='hover:text-orange-400'>LogIn</Link>
                         }
